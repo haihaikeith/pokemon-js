e6b46a9ccfe1668f2c55f11401768b6d9b822a52
haimynameiskeith <68352456+haimynameiskeith@users.noreply.github.com>
2020-07-31 10:24:46 -0700

    added for loop
    added for loop per task 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,26 @@
 var pokemonList = [
     {
-        name: 'Bulbasaur',
-        height: 0.7,
-        type: ['Grass', 'Poison']
+        pokemonName: 'Bulbasaur',
+        pokemonHeight: 0.7,
+        pokemonType: ['Grass', 'Poison']
     },
 
     {
-        name: 'Charmander',
-        height: 0.6,
-        type: 'Fire'
+        pokemonName: 'Charmander',
+        pokemonHeight: 0.6,
+        pokemonType: 'Fire'
     },
 
     {
-        name: 'Squirtle',
-        height: 0.6,
-        type: 'Water'
+        pokemonName: 'Squirtle',
+        pokemonHeight: 0.5,
+        pokemonType: 'Water'
     },
 ];
+
+for ( let i = 0; i < pokemonList.length; i++) {
+    document.write('<h2>' + pokemonList[i].pokemonName + '</h2>' + ' Height: ' + pokemonList[i].pokemonHeight)
+  if (pokemonList[i].pokemonHeight <= 0.5) {
+    document.write(' (Wow that\'s real small!)' )
+    }
+}
