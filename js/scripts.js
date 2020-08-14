8d5be8563e68fcf41a9054bd3a36fdf1385689bc
haihaikeith <kikiinthekeester@gmail.com>
2020-08-13 18:19:29 -0700

    added modal
    it doesn't work

--- js/scripts.js
+++ js/scripts.js
@@ -51,13 +51,59 @@ var pokemonRepository = (function () {
     });
   }
 
+// function to show modal
+  function showModal() {
+    var modalContainer = document.querySelector('#modal-container');
+      modalContainer.classList.add('is-visible');
+
+// clear all existing modal content
+    modalContainer.innerHTML = '';
+
+    var modal = document.createElement('div');
+    modal.classList.add('modal');
+
+// add new modal content
+    var closeButton = document.createElement('button');
+    closeButton.classList.add('modal-close');
+    closeButton.innerText = 'Get out of here';
+
+    var titleElement = document.createElement('p');
+    titleElement.innerText = title;
+
+    var buttonContent = document.createElement('p');
+    buttonContent.innerText = text;
+
+    modal.appendChild(closeButton);
+    modal.appendChild(titleElement);
+    modal.appendChild(buttonContent);
+    modalContainer.appendChild(modal);
+    modalContainer.classList.add('is-visible');
+
+  // to show the close button
+    var closeButton = document.createElement('button');
+    closeButton.classList.add('modal-close');
+    closeButton.innerText = 'Get out of here';
+    closeButton.addEventListener('click', hideModal);
+  }
+
+      document.querySelector('#show-modal').addEventListener('click', () =>{
+        showModal('Modal title', 'this should be some content');
+
+  });
+
+// hides modal
+  function hideModal() {
+    var modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible')
+  }
+
 // loads pokemon details
     function loadDetails(item) {
       var url = item.detailsUrl;
         return fetch(url).then(function (response) {
           return response.json();
     }).then(function (details) {
-// Now we add the details to the item
+// add the details to the item
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
       item.types = details.types;
@@ -65,6 +111,21 @@ var pokemonRepository = (function () {
       console.error(e);
     });
   }  
+// ESC to close out modal
+  window.addEventListener('keydown', (e) => {
+    var modalContainer = document.querySelector('#modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('if-visible')) {
+      hideModal();
+    }
+  });
+// eventListener for clicking outside the modal
+  modalContainer.addEventListener('click', (e) => {
+    var target = e.target;
+    if (target === modalContainer){
+      hideModal();
+    }
+  });
+  
     return {
       add: add,
       getAll: getAll,
